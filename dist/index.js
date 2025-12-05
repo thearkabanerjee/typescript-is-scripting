85e6ea275bd56d0e3c2bd53f78bca429e095fb31
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-05 21:17:02 +0530

    ts we go

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return (`Hi, ${name}`);
+}
+const username = 'Arka Banerjee';
+let a = greet(username);
+console.log(a);
+//# sourceMappingURL=index.js.map

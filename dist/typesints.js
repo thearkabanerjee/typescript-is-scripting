6759c96852928ef085deac4f7761de746f7d2ac6
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-06 20:29:58 +0530

    ts we go

---
+++ dist/typesints.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let drink = 'chai'; // you could have also write without the data type
+let cups = Math.random() > 0.5 ? 10 : 5;
+console.log(cups, drink);
+//# sourceMappingURL=typesints.js.map
